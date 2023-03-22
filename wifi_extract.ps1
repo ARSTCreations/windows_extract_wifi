cdf91e77a878903412acb94a8578b4bb44f44236
Rizaldy Aristyo <anandarizaldyaristyo@gmail.com>
2023-03-22 19:30:39 +0700

    Create wifi_extract.ps1

---
+++ wifi_extract.ps1
@@ -0,0 +1,10 @@
+$filename = "wifipasswd_$($Env:COMPUTERNAME)_$((Get-Date).ToString('yyyyMMdd')).txt"
+(Get-WmiObject -Class "Win32_NetworkAdapterConfiguration" | Where-Object { $_.IPEnabled -eq $true }).ForEach({
+    $profiles = (netsh -c "wlan show profiles" -Interface $_.Caption | Select-String "All User Profile\s+:\s+(.*)$" | ForEach-Object { $_.Matches.Groups[1].Value })
+    $profiles.ForEach({
+        $password = (netsh -c "wlan show profile name=`"$($_)`" key=clear" -Interface $_ | Select-String "Key Content\s+:\s+(.*)$" | ForEach-Object { $_.Matches.Groups[1].Value })
+        if ($password) {
+            "$_ : $password"
+        }
+    })
+}) | Out-File $filename
