2f31542868ae49f6b2b7ba79c4c533e24bb42d37
Rizaldy Aristyo <anandarizaldyaristyo@gmail.com>
2023-03-22 19:55:56 +0700

    added hhmmss to avoid overwriting

--- wifi_extract.ps1
+++ wifi_extract.ps1
@@ -1,4 +1,4 @@
-$filename = "wifipasswd_$($Env:COMPUTERNAME)_$((Get-Date).ToString('yyyyMMdd')).txt"
+$filename = "wifipasswd_$($Env:COMPUTERNAME)_$((Get-Date).ToString('yyyyMMddhhmmss')).txt"
 (Get-WmiObject -Class "Win32_NetworkAdapterConfiguration" | Where-Object { $_.IPEnabled -eq $true }).ForEach({
     $profiles = (netsh -c "wlan show profiles" -Interface $_.Caption | Select-String "All User Profile\s+:\s+(.*)$" | ForEach-Object { $_.Matches.Groups[1].Value })
     $profiles.ForEach({
