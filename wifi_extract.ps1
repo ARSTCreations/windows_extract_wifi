b36a6ad1eac1c08764ef4c2b8194da8193cf61db
Rizaldy Aristyo <anandarizaldyaristyo@gmail.com>
2023-04-06 19:19:52 +0700

    to hell with password only wifis

--- wifi_extract.ps1
+++ wifi_extract.ps1
@@ -3,8 +3,6 @@ $filename = "wifipasswd_$($Env:COMPUTERNAME)_$((Get-Date).ToString('yyyyMMddhhmm
     $profiles = (netsh -c "wlan show profiles" -Interface $_.Caption | Select-String "All User Profile\s+:\s+(.*)$" | ForEach-Object { $_.Matches.Groups[1].Value })
     $profiles.ForEach({
         $password = (netsh -c "wlan show profile name=`"$($_)`" key=clear" -Interface $_ | Select-String "Key Content\s+:\s+(.*)$" | ForEach-Object { $_.Matches.Groups[1].Value })
-        if ($password) {
-            "$_ : $password"
-        }
+        "$_ : $password"
     })
 }) | Out-File $filename
